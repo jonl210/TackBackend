fba4a805fbb62654750a85624c76f08bab1cbb0c
Jonathan <jonathanlambert247@gmail.com>
2019-05-19 17:18:26 -0700

    created post model

--- Tack/settings.py
+++ Tack/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'friendrequests.apps.FriendrequestsConfig',
     'groups.apps.GroupsConfig',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
---
+++ posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from datetime import datetime
+
+#Post model
+class Post(models.Model):
+    url = models.URLField()
+    u_id = models.CharField(max_length=11, default=0)
+    description = models.TextField()
+    date = models.DateTimeField(default=datetime.now)
+    by = models.ForeignKey('users.profile', on_delete=models.CASCADE, related_name="by")
+    group = models.ForeignKey('groups.group', on_delete=models.CASCADE, related_name="group")
