ff17e135356b7b6cecbb44b518ccad48f7f7834b
Jonathan <jonathanlambert247@gmail.com>
2019-06-21 05:19:47 -0700

    added code to ensure debug is false in prod

--- Tack/settings.py
+++ Tack/settings.py
@@ -23,7 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^0hkiczcae9!4e%+xo44)pv$mu-fif^g3%@qc_m(bfio5$$(8#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+if 'RDS_HOSTNAME' in os.environ:
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'www.tackapp.co']
 
